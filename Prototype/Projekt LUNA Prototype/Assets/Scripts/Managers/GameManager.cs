11b5f0d1d6dfa85ce14158faa1579cf19ed9fce1
Luke Parker <moongateuk@gmail.com>
2018-07-21 11:25:44 +0100

    Initial prototype project.

---
+++ Prototype/Projekt LUNA Prototype/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Core Update loop data.
+    private ManagedObjectBehaviour[] _currentObjects;
+    private bool _hasLoadedObjects = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    RefreshObjects();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (_hasLoadedObjects)
+	    {
+	        for (var i = 0; i < _currentObjects.Length; i++)
+	        {
+	            _currentObjects[i].UpdateMe();
+	        }
+	    }
+	}
+
+    public void RefreshObjects()
+    {
+        _hasLoadedObjects = false;
+        _currentObjects = GetComponents<ManagedObjectBehaviour>();
+        for (var i = 0; i < _currentObjects.Length; i++)
+        {
+            _currentObjects[i].StartMe();
+        }
+
+        _hasLoadedObjects = true;
+    }
+}
---
+++ Prototype/Projekt LUNA Prototype/Assets/Scripts/SPOMF/ManagedObjectBehaviour.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+/// <summary>
+/// Replacements to the Start() and Update() for a centerally managed loop area.
+/// </summary>
+public abstract class ManagedObjectBehaviour : MonoBehaviour
+{
+    public abstract void StartMe();
+    public abstract void UpdateMe();
+}
